cff8b80e4e0f8b00e95dd8f06775c2fd76d329c4
Victhoor <victor.carvalhobastos@gmail.com>
2021-04-11 22:15:11 -0300

    Ajustes no exercicio 3

--- atividade_continua_2/src/AC02_poo/ContaBancaria.java
+++ atividade_continua_2/src/AC02_poo/ContaBancaria.java
@@ -1,19 +1,32 @@
+/* 
+	Arthur Alves Correa - 1903356
+	Gabriel Tomaz do Nascimento - 1903811 
+	Eduardo Akira Hanada Oushiro - 1903669
+	Matheus Pollini Malaquias - 1903942
+	Victor Bastos De Carvalho - 1903491
+	William Alves Ribeiro - 1903690
+*/
+
 package AC02_poo;
 
-public class ContaBancaria {
-    private static int ultimoNumeroConta = 1000; // ï¿½ltimo nï¿½mero de conta utilizado
-    private String correntista;
-    private int numeroConta; // nï¿½mero da conta
-    private double saldo; // saldo da conta
-    private static double cpmfTaxa = 0.0025; // Atributo da classe e nÃ£o do objeto
-    private double cpmf; // Acumulo pago
+import java.util.Random;
 
+public class ContaBancaria {
+    private static int ultimoNumeroConta = 1000; // ultimo numero de conta utilizado
+    private String correntista;
+    private int numeroConta; // numero da conta
+    private double saldo; // saldo da conta
+    private String senha;
+    private static double cpmfTaxa = 0.0025; // taxa da cpmf, atributo pertence a classe, não ao objeto
+    private double cpmf; // Acumulo pago
 
     public ContaBancaria(String correntista, double saldo) {
         ultimoNumeroConta++;
         this.numeroConta = ultimoNumeroConta;
         this.saldo = saldo;
         this.correntista = correntista;
+        // Senha gerada automaticamente
+        this.senha = criarSenha();
         this.setCpmf(0);
     }
 
@@ -38,7 +51,21 @@ public class ContaBancaria {
     public int getNumeroConta() {
         return this.numeroConta;
     }
+    
+    private String criarSenha() {
+    	Random ran = new Random();
 
+		String[] letras ={"0","1","2","3","4","5","6","7","8","9"};
+		String senha = "";
+	  
+		for ( int i = 0; i < 6; i++){
+			int a = ran.nextInt(letras.length);
+			senha += letras[a];
+		}
+		return senha;  
+	}
+	
+    // Descontando cpmf do saldo da conta
     public void cpmf() {
         double taxa = this.saldo * cpmfTaxa;
         this.saldo -= taxa;
@@ -48,12 +75,18 @@ public class ContaBancaria {
     public String toString() {
         return "Conta de " + this.correntista + " - Saldo de R$ " + this.saldo;
     }
-
+    
+    // Buscar valor acumulado da cmpf
 	public double getCpmf() {
 		return cpmf;
 	}
 
 	private void setCpmf(double cpmf) {
 		this.cpmf = cpmf;
+	}
+	
+	// Buscar senha do objeto
+	public String getSenha() {
+		return senha;
 	}
 }
