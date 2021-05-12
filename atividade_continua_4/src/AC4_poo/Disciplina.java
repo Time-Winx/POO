5a56682a9bec6cd685baaa76692234d0163e4726
Sandra <william.war.90@gmail.com>
2021-05-12 19:43:59 -0300

    estrutura da AC4

---
+++ atividade_continua_4/src/AC4_poo/Disciplina.java
@@ -0,0 +1,39 @@
+package AC4_poo;
+
+public class Disciplina {
+	private String nome;
+	private String ementa;
+	private ArrayList<Aluno> alunos;
+	
+	public Disciplina(String nome, String ementa) {
+		
+	}
+	
+	public void incluirAluno(Aluno aluno) {
+		
+	}
+
+	public String getNome() {
+		
+	}
+
+	public String getEmenta() {
+		
+	}
+
+	public ArrayList<Aluno> getAlunos() {
+		
+	}
+
+	public void setNome(String nome) {
+	
+	}
+
+	public void setEmenta(String ementa) {
+	
+	}
+
+	public void setAlunos(ArrayList<Aluno> alunos) {
+	
+	}
+}
---
+++ atividade_continua_4/src/AC4_poo/Pessoa.java
@@ -0,0 +1,27 @@
+package AC4_poo;
+
+public class Pessoa {
+	private String nome;
+	private String nascimento;
+	
+	public Pessoa(String nome, String nascimento) {
+		this.nome = nome;
+		this.nascimento = nascimento;
+	}
+	
+	public String getNome() {
+		
+	}
+	
+	public String getNascimento() {
+		
+	}
+	
+	public void setNome(String nome) {
+		
+	}
+	
+	public void setNascimento(String nascimento) {
+		
+	}
+}
